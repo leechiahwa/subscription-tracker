84dc1ffe88ff07a694e8f87572a30817ad5d5334
leechiahwa <leechiahwa99@hotmail.com>
2024-11-01 12:22:02 +0800

    Add tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
@@ -5,6 +6,11 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   base: "/",
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   preview: {
     port: 8080,
     strictPort: true,
